405dfb8a4b4bb15cc63f9d292cde7ac557bfbbd5
guyskk <1316792450@qq.com>
2016-03-30 11:44:17 +0800

    update captcha

--- kkblog/captcha.py
+++ kkblog/captcha.py
@@ -1,4 +1,15 @@
 # coding=utf-8
+"""
+无状态验证码
+0. 服务端先配置一个密钥KEY
+1. 客户端请求验证码TOKEN
+   服务端生成一个随机字符串TEXT，再生成一个SALT，用KEY和SALT对TEXT进行加密
+   加密后的TEXT和SALT拼接到一起，作为TOKEN返回给客户端
+2. 客户端请求验证码图片，携带验证码TOKEN
+   服务端根据KEY和TOKEN中的SALT，解密出TEXT，用这个TEXT生成一幅图片
+3. 客户端发送业务请求，携带验证码TOKEN和用户输入的CODE
+   服务端根据KEY和TOKEN中的SALT，解密出TEXT，和CODE比对
+"""
 from __future__ import unicode_literals, absolute_import, print_function
 import random
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
@@ -12,11 +23,12 @@ from flask import current_app, make_response, url_for
 from flask_restaction import Resource
 
 
-def encrypt(text, key, salt):
+def encrypt(text, key):
     """加密函数，如果text不是16的倍数【加密文本text必须为16的倍数！】，那就补足为16的倍数.
     这里密钥salt 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
     text: byte string
     """
+    salt = gen_salt(16)
     cryptor = AES.new(key, mode=AES.MODE_CBC, IV=salt)
     length = 16
     count = len(text)
@@ -25,21 +37,24 @@ def encrypt(text, key, salt):
     ciphertext = cryptor.encrypt(text)
     # 因为AES加密时候得到的字符串不一定是ascii字符集的，输出到终端或者保存时候可能存在问题
     # 所以这里统一把加密后的字符串转化为16进制字符串
-    return b2a_hex(ciphertext)
+    return "%s$%s" % (b2a_hex(ciphertext), salt)
 
 
-def decrypt(text, key, salt):
-    """解密后，去掉补足的空格用strip() 去掉
+def decrypt(text, key):
+    """解密后，用strip()去掉补足的空格
     text: byte string
     """
+    if text.count('$') < 1:
+        return False
+    token, salt = text.split('$', 1)
     cryptor = AES.new(key, mode=AES.MODE_CBC, IV=salt)
-    plain_text = cryptor.decrypt(a2b_hex(text))
+    plain_text = cryptor.decrypt(a2b_hex(token))
     return plain_text.rstrip('\0')
 
 
-def check_captcha(token, text, key, salt):
+def check_captcha(token, text, key):
     try:
-        text2 = decrypt(token, key, salt)
+        text2 = decrypt(token, key)
         return text.lower() == text2.lower()
     except:
         return False
@@ -137,27 +152,10 @@ def create_captcha(text, font_type, height=30, mode="RGB",
     img = create_twisty(img)
     return img
 
-# salt, exp
-CAPTCHA_SALT = None, None
-CHARS = string.ascii_letters + string.digits
-
-
-def get_salt():
-    """返回salt。如果salt过期了，更新salt。"""
-    global CAPTCHA_SALT
-    salt, exp = CAPTCHA_SALT
-    if not salt or datetime.utcnow() > exp:
-        salt = gen_salt(16)
-        sec = current_app.config["CAPTCHA_SALT_EXP"]
-        exp = datetime.utcnow() + timedelta(seconds=sec)
-        CAPTCHA_SALT = salt, exp
-    return salt
-
 
 def check(token, text):
     key = current_app.config["CAPTCHA_KEY"]
-    salt = get_salt()
-    return check_captcha(token, text, key, salt)
+    return check_captcha(token, text, key)
 
 
 class Captcha(Resource):
@@ -190,7 +188,7 @@ class Captcha(Resource):
         """获取一个验证码链接"""
         key = current_app.config["CAPTCHA_KEY"]
         text = ''.join(random.sample(CHARS, 4))
-        token = encrypt(text, key, get_salt())
+        token = encrypt(text, key)
         token = token.encode("ascii")
         return {
             "token": token,
@@ -202,7 +200,7 @@ class Captcha(Resource):
         key = current_app.config["CAPTCHA_KEY"]
         token = token.encode("ascii")
         try:
-            text = decrypt(token, key, get_salt())
+            text = decrypt(token, key)
             assert len(text) == 4, "解密失败"
         except:
             return "token无效", 404
