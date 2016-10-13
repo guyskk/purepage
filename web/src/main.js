1c4ecd818db9bb768be98ef7f8c076836f3174dd
guyskk <guyskk@qq.com>
2016-10-13 18:01:56 +0000

    add article

--- web/src/main.js
+++ web/src/main.js
@@ -10,17 +10,28 @@ Vue.use(Vuex)
 import Signup from './comps/signup'
 import Index from './comps/index'
 import Login from './comps/login'
+import Article from './comps/article'
 
 const routes = [
   { path: '/', component: Index },
   { path: '/signup', component: Signup },
   { path: '/login', component: Login },
+  { path: '/view/article', component: Article },
   { path: '*', redirect: '/' },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   routes: routes,
+})
+
+// 检查登录状态
+router.afterEach(() => {
+  res.user.get_me().then(data => {
+    console.log(data.username)
+  }).catch(() => {
+    router.replace('/login')
+  })
 })
 
 // 修复Material Design Lite动态效果
@@ -35,10 +46,5 @@ router.afterEach(() => {
 new Vue({
   el: '#app',
   router: router,
-  render: h => h(App),
-  methods: {
-    go(url) {
-      this.$router.push(url)
-    }
-  }
+  render: h => h(App)
 })
